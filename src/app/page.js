a391baf39fa21d63b476bff7986e40bba3937bb8
Nathan Wang <nathan123wang@gmail.com>
2023-05-13 18:29:06 +1000

    Push

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,27 @@
 import Navbar from "../../components/navbar";
 import "./globals.css";
-import "../../components/header/header.js";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <header />
       <div className="landing-section">
         <div className="landing-content">
           <video src="/images/Animation2.mp4" autoPlay muted loop className="landing-video" style={{ width: "50%", height: "auto" }} />
-          <div className="landing-text">GoTask - Automated calendar task automation and gamifying</div>
+          <div className="landing-text">
+            GoTask
+            <div className="text-2">Automated calendar task automation and gamifying</div>
+          </div>
         </div>
       </div>
+      <div className="landing-section">
+      <div className="blue"></div>
+        <div className="landing-content">
+          <video src="/images/Animation3.mp4" autoPlay muted loop className="landing-video" style={{ width: "50%", height: "auto" }} />
+          <div className="landing-text">Another section with different content</div>
+        </div>
+      </div>
+      <div className="blue-circley"></div>
     </div>
   );
 }
