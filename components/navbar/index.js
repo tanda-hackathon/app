1440c0c63c9815602705309b6576043588935e4a
Nathan Wang <nathan123wang@gmail.com>
2023-05-13 16:38:45 +1000

    asd

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,12 +1,11 @@
 "use client";
-import profile from './profile.jpeg';
+
 import { useEffect } from "react";
 import Logo from "./components/Logo";
 import SignUp from "./components/Signup";
 import { useRouter, usePathname } from "next/navigation";
 import { useState } from "react";
-import { useSelector } from "react-redux";
-import "./style.css"
+import "./style.css";
 
 const routeName = {
   "/": "Login",
@@ -15,20 +14,60 @@ const routeName = {
 };
 
 export default function Navbar() {
-    const router = useRouter()
-    const CurrentPage = usePathname()
-    const [pageName, setPageName] = useState()
-    const userPoint = useSelector((state) => state.user.point);
+  const router = useRouter();
+  const CurrentPage = usePathname();
+  const [pageName, setPageName] = useState();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    useEffect(() => {
-        setPageName(routeName[CurrentPage])
-    }, [CurrentPage])
-    return(
-        <nav className="navbar">
-            <div className="navbar-container">
-                <Logo/>
-                {`Current Page: ${pageName} | User Point: ${userPoint}`}
-                <SignUp/>
+  useEffect(() => {
+    setPageName(routeName[CurrentPage]);
+  }, [CurrentPage]);
+
+  const handleProfileClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleCoinsClick = () => {
+    console.log("Coins clicked");
+  };
+
+  const handleShopClick = () => {
+    console.log("Shop clicked");
+  };
+
+  const handleProfileMenuClick = () => {
+    console.log("Profile menu clicked");
+  };
+
+  const handleLogoutClick = () => {
+    console.log("Logout clicked");
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Logo />
+        <div className="profile-dropdown">
+          <img
+            src="path/to/profile-image.jpg"
+            alt="Profile"
+            className="profile-image"
+            onClick={handleProfileClick}
+          />
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              <div className="dropdown-item" onClick={handleCoinsClick}>
+                Coins
+              </div>
+              <div className="dropdown-item" onClick={handleShopClick}>
+                Shop
+              </div>
+              <div className="dropdown-item" onClick={handleProfileMenuClick}>
+                Profile
+              </div>
+              <div className="dropdown-item" onClick={handleLogoutClick}>
+                Logout
+              </div>
             </div>
           )}
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
 "use client";
 
 import Navbar from "../../components/navbar";
-import '../../data/images/Landinglottie'
-import Landinglottie from '../../data/images/Animation.mp4';
+//import '../../data/images/Landinglottie'
+//import Landinglottie from '../../data/images/Animation.mp4';
 
 export default function Home() {
-  const something = useSelector((state) => state.user.point);
   return (
     <div>
         <Navbar/>
-
     </div>
   )
 }
