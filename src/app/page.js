c12a25936ade6c356cd8c88e1a24d8f93dbe352d
Luke Mai <mailong.c8.cva@gmail.com>
2023-05-13 16:11:50 +1000

    Adding store

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+"use client";
+
 import Navbar from "../../components/navbar";
+
+import { useSelector } from "react-redux";
 
 
 export default function Home() {
+  const something = useSelector((state) => state.user.point);
   return (
     <div>
-        <Navbar/>
     </div>
   )
 }
